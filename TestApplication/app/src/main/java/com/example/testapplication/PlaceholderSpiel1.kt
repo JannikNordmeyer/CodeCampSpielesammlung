8d164cf41a3117e20e264ee963850fc3026cef52
JadaMaar <42937194+JadaMaar@users.noreply.github.com>
2022-03-01 17:18:11 +0100

    API call finished

--- TestApplication/app/src/main/java/com/example/testapplication/PlaceholderSpiel1.kt
+++ TestApplication/app/src/main/java/com/example/testapplication/PlaceholderSpiel1.kt
@@ -1,5 +1,6 @@
 package com.example.testapplication
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -17,57 +18,73 @@ import com.esri.arcgisruntime.mapping.view.MapView
 import android.content.Context
 import java.io.IOException
 import android.util.Log
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.*
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import org.json.JSONObject
 import java.io.InputStream
+import kotlin.random.Random
 
 class PlaceholderSpiel1 : Fragment() {
     private val TAG = PlaceholderSpiel1::class.java.simpleName
 
-    private val fragmentPlaceholderspiel1Binding by lazy {
-        FragmentPlaceholderspiel1Binding.inflate(layoutInflater)
-    }
+    //private val fragmentPlaceholderspiel1Binding by lazy {        FragmentPlaceholderspiel1Binding.inflate(layoutInflater)    }
 
-    private val mapView: MapView by lazy {
-        fragmentPlaceholderspiel1Binding.mapView
-    }
+    //private val mapView: MapView by lazy { fragmentPlaceholderspiel1Binding.mapView    }
 
     private lateinit var binding: FragmentPlaceholderspiel1Binding
+
+    lateinit var targetLocation: JSONObject
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?): View? {
 
-        //binding = FragmentPlaceholderspiel1Binding.inflate(inflater,container,false)
-        val view = fragmentPlaceholderspiel1Binding.root
+        binding = FragmentPlaceholderspiel1Binding.inflate(inflater,container,false)
+        //val view = fragmentPlaceholderspiel1Binding.root
+        val view = binding.root
         val viewmodel = ViewModelProvider(requireActivity()).get(PlaceholderSpiel1ViewModel::class.java) //Shared Viewmodel w/ GameHolder
 
         // DEIN CODE HIER
         //++setContentView(fragmentPlaceholderspiel1Binding.root)
 
-        setApiKeyForApp()
+        apiCall()
 
-        setupMap()
+        binding.idBtnGenerate.setOnClickListener {
+            apiCall()
+        }
+        //setApiKeyForApp()
 
-        Log.d(TAG, resources.openRawResource(R.raw.kassel).toString())
-
+        //setupMap()
 
         return view
     }
 
-    override fun onPause() {
-        mapView.pause()
-        super.onPause()
+    private fun apiCall() {
+        val url = "https://geoportal.kassel.de/arcgis/rest/services/Service_Daten/Freizeit_Kultur/MapServer/0/query?where=1%3D1&text=&objectIds=&time=&" +
+                "geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&distance=&units=esriSRUnit_Foot&relationPar" +
+                "am=&outFields=*&returnGeometry=true&returnTrueCurves=false&maxAllowableOffset=&geometryPrecision=&outSR=&havingClause=&retu" +
+                "rnIdsOnly=false&returnCountOnly=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&returnZ=false&returnM=false&gdbVers" +
+                "ion=&historicMoment=&returnDistinctValues=false&resultOffset=&resultRecordCount=&returnExtentOnly=false&datumTransformation=&par" +
+                "ameterValues=&rangeValues=&quantizationParameters=&featureEncoding=esriDefault&f=geojson"
+        val queue = Volley.newRequestQueue(activity)
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET,
+            url,
+            null,
+            Response.Listener {
+                val rand = Random.nextInt(0,132)
+                targetLocation = it.getJSONArray("features").getJSONObject(rand)
+                Log.d("MainActivity", "test: " + targetLocation.toString())
+            }, Response.ErrorListener {
+                Log.d("MainActivity", "Api call failed")
+            }
+        )
+        queue.add(jsonObjectRequest)
     }
 
-    override fun onResume() {
-        super.onResume()
-        mapView.resume()
-    }
-
-    override fun onDestroy() {
-        mapView.dispose()
-        super.onDestroy()
-    }
 
     private fun setApiKeyForApp(){
         // set your API key
@@ -79,7 +96,7 @@ class PlaceholderSpiel1 : Fragment() {
     }
 
     // set up your map here. You will call this method from onCreate()
-    private fun setupMap() {
+    /*private fun setupMap() {
 
         // create a map with the BasemapStyle streets
         val map = ArcGISMap(BasemapStyle.ARCGIS_TOPOGRAPHIC)
@@ -89,7 +106,7 @@ class PlaceholderSpiel1 : Fragment() {
 
         // set the viewpoint, Viewpoint(latitude, longitude, scale)
         mapView.setViewpoint(Viewpoint(34.0270, -118.8050, 72000.0))
-    }
+    }*/
 
     fun readJSONFromAsset(): String? {
         var json: String? = null
